2960a62bbbcdffbfd9f584c4d6e23edd9947da48
quannguyen <quanmanchester0405@gmail.com>
2025-04-19 17:18:09 +0700

    feat : handle conflict

---
+++ src/app/home/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page333 = () => {
+  return <div>Page333</div>;
+};
+
+export default Page333;
---
+++ src/app/viewPDF/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { PDFViewerTest } from "../pdf/page";
+
+const ViewPDF = () => {
+  return (
+    <div>
+      <PDFViewerTest />
+    </div>
+  );
+};
+
+export default ViewPDF;
